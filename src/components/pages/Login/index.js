169423b94f7f61ffa483823321f34d038e2c138e
anti009x <riskisuleman76@gmail.com>
2024-01-08 13:52:33 +0700

    update shooting stars

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -4,16 +4,43 @@ import gmail from '../../img/gmail.png'
 
 import welcome from '../../img/welcome.jpg'
 
+import style from './style.css'
+
 
 
 
 const Login = () => {
   return (
     <>
+
+<div className='.body'>
+ <div class="night">
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   <div class="shooting_star"></div>
+   </div>
+
     <section className="vh-100">
   <div className="container py-5 h-100">
     <div className="row d-flex align-items-center justify-content-center h-100">
-      <div className="col-md-8 col-lg-7 col-xl-6 shadow p-3 bg-white rounded mb-md-3 mb-sm-4 mb-2">
+      <div className="col-md-8 col-lg-7 col-xl-6 shadow p-3 bg-white rounded mb-md-3 mb-sm-4 mb-2 mt-2">
         <img
           src={welcome}
           className="img-fluid"
@@ -67,11 +94,11 @@ const Login = () => {
             Sign in
           </button>
           <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+            <p className="text-center fw-bold mx-3 mb-0 text-white" >OR</p>
           </div>
 
           <div
-                 className="btn btn-primary btn-lg btn-block"
+                 className="btn btn-primary btn-lg btn-block mt-2"
                  style={{ backgroundColor: "#C0C2C9" }}
 
                  role="button"
@@ -79,7 +106,7 @@ const Login = () => {
           <img src={gmail}  style={{width:20,height:20}} >
 
           </img>
-            <i className="ml-2" />
+            <i className="ml-2 " />
             Continue with Email
 
           </div>
@@ -89,7 +116,9 @@ const Login = () => {
   </div>
 </section>
 
-    
+
+</div>
+
     </>
   )
 }
